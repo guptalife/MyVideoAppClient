bb1131d88e3786f0fba94057621a1a4212dbde87
Abhishek Gupta <yoyoabhishek7966@gmail.com>
2022-11-04 02:30:19 +0530

    video server

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import store from '../store/store';
 
-const BASE_URL = "https://videos-call-server-app.herokuapp.com//api" 
+const BASE_URL = "https://videos-call-server-app.herokuapp.com/api" 
 
 const api = axios.create({
     baseURL:BASE_URL
